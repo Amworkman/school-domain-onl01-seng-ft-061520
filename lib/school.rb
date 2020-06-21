8113cb9b4a1847b398419000532d8da84f2c1cc2
Adam Workman <adammworkman25@gmail.com>
2020-06-21 23:05:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,9 @@ class School
 def initialize(name)
   @name = name
   
-  @roster = {} #keys of grade levels... value of each key will be an arry of student names
+  @roster = {}
+  
+    #keys of grade levels... value of each key will be an arry of student names
    
     #   hash["new_key"] = []
     # hash["new_key"] << "new_value_for_value_array"
@@ -22,10 +24,16 @@ end
     self.roster[grade] << name
   end
   
-  def grade(grade)  # should take in an argument of a grade and return all of the students in that grade
+  def grade(grade) 
     
+    # should take in an argument of a grade and return all of the students in that grade
+    
+    self.roster[grade]
   end
   
-  #You should be able to get a sorted list of all the students where the strings in the student arrays are sorted alphabetically
-  
+  def sorted_list
+    
+    #You should be able to get a sorted list of all the students where the strings in the student array are sorted alphabetically
+    
+    
 end
